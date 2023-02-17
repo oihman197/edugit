8ada2322aeb50818981d6523d95ab27ff5ddb220
oihman197 <robinurohman197@gmail.com>
2023-02-18 01:29:56 +0700

    tugas

--- tugas.c.commands.cy.js
+++ tugas.c.commands.cy.js
@@ -1,24 +1,43 @@
-describe('test zero.webappsecurity', () => {
-    it('visit web', () => {
-    cy.visit('http://zero.webappsecurity.com/login.html');
-    cy.url().should('include', 'login.html')
-    });
+describe('test zero.webappsecurity', function() {
+    beforeEach(() => {
+        cy.visit('http://zero.webappsecurity.com/login.html');
+        cy.url().should('include', 'login.html')
+    })
 
     it('Should fill Login', () => {
-        cy.visit('http://zero.webappsecurity.com/login.html');
-        cy.get('#user_login').wait(1000).clear()
-        cy.get('#user_login').wait(1000).type('username')
-        cy.get('#user_password').wait(1000).clear()
-        cy.get('#user_password').wait(1000).type('password')
-        cy.get('#user_remember_me').wait(1000).click()
-        cy.get('#user_remember_me').wait(1000).type('keep me signed in')
-        cy.get('.btn').wait(1000).submit
-        cy.get('.btn').wait(1000).click()
-        cy.get('#pay_bills_tab').wait(1000).click()
-        cy.get('#sp_payee').wait(1000).select('bofa')
-        cy.get('#sp_account').wait(1000).select("5")
-        cy.get('#sp_amount').wait(1000).type('50')
-        cy.get('#sp_date').wait(1000).type('2023-02-15')
-        cy.get('#pay_saved_payees').wait(1000).submit
+        cy.get('#user_login').submit
+        cy.get('#user_login').type('username')
+        cy.get('#user_password').submit
+        cy.get('#user_password').type('password')
+        cy.get('#user_remember_me').click()
+        cy.get('#user_remember_me').type('keep me signed in')
+        cy.get('.btn').submit
+        cy.get('.btn').click()
+        
     });
-})
+
+    it('Should fill to payment',() => {
+        cy.get('#user_login').submit
+        cy.get('#user_login').type('username')
+        cy.get('#user_password').submit
+        cy.get('#user_password').type('password')
+        cy.get('#user_remember_me').click()
+        cy.get('#user_remember_me').type('keep me signed in')
+        cy.get('.btn').submit
+        cy.get('.btn').click()
+        cy.get('#pay_bills_tab').submit
+        cy.get('#pay_bills_tab').click()
+        // cy.get('#sp_payee').wait(1000).select('bofa')
+        // cy.get('#sp_account').wait(1000).select("5")
+        // cy.get('#sp_amount').wait(1000).type('50')
+        // cy.get('#sp_date').wait(1000).type('2023-02-15')
+        cy.payment('bofa', "5", '50', '2023-02-15')
+        cy.get('#pay_saved_payees').submit
+        cy.contains('pay').click()
+       
+        cy.get('#pay_saved_payees').click();
+        cy.get('#alert_content > span').should('be.visible');
+        cy.get('#alert_content > span').should('have.text', 'The payment was successfully submitted.');
+        
+    });
+});
